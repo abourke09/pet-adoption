f04ce139e6a7fb9be8e621ffc256555faf6df2f4
abourke09 <aabourke@gmail.com>
2019-03-13 14:35:40 -0700

    testing changes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <p>Welcome to Annie's App</p>
           <a
             className="App-link"
             href="https://reactjs.org"
